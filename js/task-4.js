deae767dee4e6069b95946c35575b0cf823de68d
Irina Kirillova <keira.kirillova@gmail.com>
2024-06-21 19:46:33 +0200

    add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmit = e => {
+  e.preventDefault();
+  const target = e.target;
+  const email = target.elements.email.value.trim();
+  const password = target.elements.password.value.trim();
+
+  if (email && password) {
+    const formData = { email, password };
+    console.log(formData);
+    target.reset();
+  } else {
+    alert('All form fields must be filled in');
+  }
+};
+
+form.addEventListener('submit', onFormSubmit);
