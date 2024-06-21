a1e4a282f1253e11dd2b8e119dca02bee7a2c576
Irina Kirillova <keira.kirillova@gmail.com>
2024-06-21 19:55:06 +0200

    add task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const button = document.querySelector('.change-color');
+const output = document.querySelector('.color');
+
+const onButtonClick = () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  output.textContent = color;
+};
+
+button.addEventListener('click', onButtonClick);
